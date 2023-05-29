bcaf5046db8654351f07ae411eca8337ade9ace3
josegabrielfc <jgfchona@gmail.com>
2023-05-29 10:31:08 -0500

    First commit

---
+++ src/main/java/com/ufps/web/Parcial2WebApplication.java
@@ -0,0 +1,14 @@
+package com.ufps.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial2WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial2WebApplication.class, args);
+		System.out.println("Compila");
+	}
+
+}
---
+++ src/test/java/com/ufps/web/Parcial2WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufps.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
